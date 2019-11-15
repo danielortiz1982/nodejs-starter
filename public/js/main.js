eb152d0efbe8cd8ccc40d1c32c431dc60aa93dba
Daniel Ortiz <dortiz@Daniels-MBP.lan1>
2019-11-15 08:12:48 -0500

    update to repo

--- public/js/main.js
+++ public/js/main.js
@@ -5,19 +5,20 @@ window.document.title = 'Nodejs and MongoDB Starter Project'
 // demo geo location script...
 var x = document.getElementById("demo");
 
-function getLocation() {
+// function getLocation() {
 
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else { 
-    x.innerHTML = "Geolocation is not supported by this browser.";
-  }
-}
+//   if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(showPosition);
+//   } else { 
+//     x.innerHTML = "Geolocation is not supported by this browser.";
+//   }
+// }
 
-function showPosition(position) {
-  x.innerHTML = "Latitude: " + position.coords.latitude + 
-  "<br>Longitude: " + position.coords.longitude;
-}
+// function showPosition(position) {
+//   console.log(position)
+//   x.innerHTML = "Latitude: " + position.coords.latitude + 
+//   "<br>Longitude: " + position.coords.longitude;
+// }
 
 
-console.log(window.navigator.userAgent)
+// console.log(window.navigator.userAgent)
