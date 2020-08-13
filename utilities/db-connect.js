6c69158d1f8b1540cf1acfc4e512a68d9fc5625d
danielortiz1982 <danielortiz1982@gmail.com>
2020-08-13 11:28:36 -0400

    update to db connect

--- utilities/db-connect.js
+++ utilities/db-connect.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 
 const database  = 'nodejs-starter'
 const appPort   = 27017
-const dbConnection = `mongodb://127.0.0.1:${appPort}/${database}`
+//const dbConnection = `mongodb://127.0.0.1:${appPort}/${database}`
+const dbConnection = `mongodb://<bv>:<br@nd!ngV3rt!c@l$>@ds147592.mlab.com:47592/bv-nodejs-starter`
 
 mongoose.connect(dbConnection, {
     useNewUrlParser: true,
@@ -10,3 +11,8 @@ mongoose.connect(dbConnection, {
     useFindAndModify: false
 })
 
+// sample db configs 
+// user - bv
+// password - br@nd!ngV3rt!c@l$
+// connection shring
+// mongodb://<dbuser>:<dbpassword>@ds147592.mlab.com:47592/bv-nodejs-starter
